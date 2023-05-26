7b7b3368c4c62a197a3777fc4641211c361f214c
Reuven Martin <jaeger33@gmail.com>
2023-05-26 17:17:17 -0400

    some refactoring and minor changes

--- web/script.js
+++ web/script.js
@@ -6,137 +6,197 @@ const getNewChecklist = async () => {
     return await data.json();
 }
 
-const months = ["January", "February", "March", "April", 
+const getNRandomFromArray = (arr, n) => {
+    let newArr = [];
+    for (let i = 0; i < n; i++) {
+        let index;
+        do {
+            index = Math.floor(Math.random() * arr.length);
+        } while (newArr.includes(arr[index]));
+        newArr.push(arr[index]);
+    }
+    return newArr;
+}
+
+const createGame = (rounds) => {
+    let score = 0;
+    let curRound = 1
+    let answerHandler;
+
+    const winRound = won => {
+        score += won ? 1 : 0;
+        curRound++;
+    }
+
+    const doneGame = () => {
+        return curRound > rounds;
+    }
+
+    const getScore = () => {
+        return score;
+    }
+
+    const getRound = () => {
+        return curRound;
+    }
+
+    const getRoundCount = () => {
+        return rounds;
+    }
+
+    return {winRound, doneGame, getScore, getRound, getRoundCount};
+}
+
+const MONTHS = ["January", "February", "March", "April", 
                 "May", "June", "July", "August",
                 "September", "October", "November", "December"];
 
-const createElement = (eleType, eleId, textContent, cssClass) => {
+/*const createElement = (eleType, eleId, textContent, cssClass) => {
     let newEle = document.createElement(eleType);
     newEle.id = eleId
     if (textContent) newEle.textContent = textContent;
     if (cssClass) newEle.setAttribute("class", cssClass);
     return newEle;
+}*/
+
+const createElement = (options) => {
+    let newEle = document.createElement(options.type);
+    if (options.id) newEle.id = options.id
+    if (options.textContent) newEle.textContent = options.textContent;
+    if (options.cssClass) {
+        options.cssClass.forEach(cssClass => {
+            newEle.classList.add(cssClass);
+        });
+    }
+    if (options.href) newEle.href = options.href;   
+    return newEle;
+}
+
+const getFourMonths = (month) => {
+    let monthsOutput = [];
+    monthsOutput.push({month: month, correct: true});
+    let otherMonths = MONTHS.filter(m => m != month);
+    otherMonths = getNRandomFromArray(otherMonths, 3);
+    otherMonths.forEach(m => {
+        monthsOutput.push({month: m, correct: false});
+    });
+    monthsOutput.sort((a, b) => MONTHS.indexOf(a.month) - MONTHS.indexOf(b.month));
+    return monthsOutput;
 }
 
 const generateMonthConfig = checklist => {
-    console.log(checklist);
     let answerMonth = parseInt(checklist.day.split("-")[1] - 1);
-
-    let answers = [];
-    answers.push({val: answerMonth, correct:true});
-
-    let valInAnswers = val => {
-        for (let answer of answers) {
-            if (answer.val == val) return true;
-        }
-        return false;
-    };
-
-    for (let i = 0; i < 3; i++) {
-        let val;
-        do {
-            val = Math.floor(Math.random() * 12);
-        } while (valInAnswers(val))
-        answers.push({val: val, correct: false});
-    }
-
-    answers.sort((a, b) => a.val - b.val);
-    textAnswers = [];
-    answers.forEach(answer => {
-        textAnswers.push({text: months[answer.val], correct: answer.correct});
-    });
-
-    return {
-        hidden: [{field: "month"}],
-        answers: textAnswers
-    }
+    let answers = getFourMonths(MONTHS[answerMonth]);
+    return answers;
 }
 
-const generateAnswers = (data, quizType) => {
+const generateAnswers = (data, quizType, game) => {
     
-    let quizAnswers = createElement("section", "answers");
-    let config = generateMonthConfig(data);
+    let quizAnswers = createElement({type: "div", id: "answers"});
+    let answers = generateMonthConfig(data);
 
-    config.answers.forEach(answer => {
-        let button = createElement("button", answer.correct ? "right-answer" : "wrong-answer", answer.text, "answer");
-        button.classList.add("answer-unknown");
+    answers.forEach(answer => {
+        let button = createElement({
+            type: "button",
+            textContent: answer.month,
+            cssClass: ["answer", "answer-unknown"]
+        });            
+        button.dataset.correct = answer.correct;
         quizAnswers.appendChild(button);
-        button.addEventListener("click", answerHandler);
+        button.addEventListener("click", game.answerHandler);
     });
     return quizAnswers;
 };
 
-const answerHandler = e => {
+const answerHandler = (game, e) => {
     e.target.classList.add("selected");
-    answerUpdate();
+    game.winRound(e.target.classList.contains("right-answer")); 
+    answerUpdate(game);
 };
 
 const generateQuestion = (data, quizType) => {
-    let quizQuestion = createElement("section", "question-data");
+    let quizQuestion = createElement({type: "section", id: "question-data"});
 
-    let locationDiv = createElement("div", "location-data");
+    let locationDiv = createElement({type: "div", id: "location-data"});
+    locationDiv.appendChild(createElement({type: "div", id: "location", textContent: data.location}));
+    locationDiv.appendChild(createElement({type: "div", id: "county", textContent: data.county}));
+    locationDiv.appendChild(createElement({type: "div", id: "province", textContent: data.province}));
     quizQuestion.appendChild(locationDiv);
 
-    locationDiv.appendChild(createElement("div", "hotspot", data.location));
-    locationDiv.appendChild(createElement("div", "county", data.county));
-    locationDiv.appendChild(createElement("div", "province", data.province));
+    quizQuestion.appendChild(createElement({type: "div", id: "observer", textContent: data.observer}));
 
-    quizQuestion.appendChild(createElement("div", "observer", data.observer));
-
-    let dateTimeDiv = createElement("div", "date-time-data");
+    let dateTimeDiv = createElement({type: "div", id: "date-time-data"});
+    let date = data.day.split("-");
+    dateTimeDiv.appendChild(createElement({type: "span", id: "day", textContent: date[2]}));
+    let month = createElement({type: "span", id: "month", textContent: MONTHS[parseInt(date[1]) - 1], cssClass: ["hidden"]});
+    dateTimeDiv.appendChild(month);
+    dateTimeDiv.appendChild(createElement({type: "span", id: "year", textContent: date[0]}));
+    dateTimeDiv.appendChild(createElement({type: "span", id: "time", textContent: data.time}));
     quizQuestion.appendChild(dateTimeDiv);
 
-    dateTimeDiv.appendChild(createElement("div", "day", data.day));
-    dateTimeDiv.appendChild(createElement("div", "month", months[data.month]));
-    dateTimeDiv.appendChild(createElement("div", "year", data.year));
-    dateTimeDiv.appendChild(createElement("div", "time", data.time));
-    quizQuestion.appendChild(dateTimeDiv);
-
-    let checklistLink = "https://ebird.org/checklist/" + data.id;
-    let linkDiv = createElement('a', "checklist-link", checklistLink);
-    linkDiv.href = checklistLink;
+    let linkDiv = createElement({
+        type: "a",
+        id: "checklist-link",
+        textContent: "https://ebird.org/checklist/" + data.id,
+        href: "https://ebird.org/checklist/" + data.id});
     quizQuestion.appendChild(linkDiv);    
 
-    let speciesDiv = createElement('ul', "species-list");
+    let speciesDiv = createElement({type: "div", id: "species-list"});
+    let speciesTable = createElement({type: "table", id: "species-table"});
+    speciesDiv.appendChild(speciesTable);
     quizQuestion.appendChild(speciesDiv);
 
     let species = data.species;
     species.forEach(obs => {
-        let obsElement = document.createElement('li');
-        obsElement.classList.add("obs-species");
-        let speciesEle = document.createElement('span');
-        speciesEle.classList.add("species");
-        speciesEle.innerText = obs.species;
-        let abundanceEle = document.createElement('span');
-        abundanceEle.classList.add("abundance");
-        abundanceEle.innerText = obs.count;
-
+        let obsElement = createElement({type: "tr", cssClass: ["obs-species"]});
+        let speciesEle = createElement({type: "td", cssClass: ["species"], textContent: obs.species});
+        let abundanceEle = createElement({type: "td", cssClass: ["abundance"], textContent: obs.count});
         obsElement.appendChild(abundanceEle);
         obsElement.appendChild(speciesEle);
-        speciesDiv.appendChild(obsElement);
+        speciesTable.appendChild(obsElement);
     });    
     
     return quizQuestion;
 };
 
-const answerUpdate = () => {
+const answerUpdate = (game) => {
     for (answer of document.querySelector("#answers").children) {
-        answer.classList.add(answer.id == "wrong-answer" ? "answer-wrong" : "answer-correct")
+        answer.classList.add(answer.dataset.correct == "false" ? "answer-wrong" : "answer-correct");
         answer.classList.remove("answer-unknown");
-        answer.removeEventListener("click", answerHandler);
+        answer.removeEventListener("click", game.answerHandler);
     }
+    document.querySelector("#month").classList.remove("hidden");
+    const nextButton = document.querySelector("#next-question");
+    nextButton.addEventListener('click', nextHandler.bind(null, game));
+    nextButton.classList.remove('disabled');
+    updateScore(game);
 }
 
-getNewChecklist().then(data => {
-    document.querySelector("#question-area").appendChild(generateQuestion(data));
-    document.querySelector("#answers-area").appendChild(generateAnswers(data));
-});
+const nextHandler = async (game) => {
+    const nextButton = document.querySelector("#next-question");
+    nextButton.classList.add('disabled');
+    nextButton.parentNode.replaceChild(nextButton.cloneNode(true), nextButton);    
 
-document.querySelector('#next-question').addEventListener('click', async e => {
     data = await getNewChecklist()
     document.querySelector("#question-area").replaceChildren(generateQuestion(data));
-    document.querySelector("#answers-area").replaceChildren(generateAnswers(data));
-});
+    document.querySelector("#answers-area").replaceChildren(generateAnswers(data, "month", game));
+}
+
+const updateScore = (game) => {
+    const score = document.querySelector('#score');
+    const curRound = document.querySelector('#cur-round');
+    const roundsLeft = document.querySelector('#rounds-left');
+    score.innerText = game.getScore();
+    curRound.innerText = game.getRound();
+    roundsLeft.innerText = game.getRoundCount() - game.getRound();
+};
+
+let game = createGame(10);
+game.answerHandler = answerHandler.bind(null, game);
+updateScore(game);
+nextHandler(game);
+
+
     
 
 
