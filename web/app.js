8dd2530240dec3cdad9e18facc264bdc0fbdd8f9
Reuven Martin <jaeger33@gmail.com>
2023-06-12 20:59:11 -0400

    add multiple game modes

--- web/app.js
+++ web/app.js
@@ -1,2 +1,14 @@
-import {Game} from './modules/game.js';
+import {createGame} from './modules/game.js';
+import {createMainMenu} from './modules/mainMenu.js';
 
+const newGame = (rounds, gameType) => {
+    document.querySelector("body").textContent = "";
+    document.querySelector("body").append(createGame(rounds, gameType, mainMenu));
+}
+
+const mainMenu = () => {
+    document.querySelector("body").textContent = "";
+    document.querySelector("body").append(createMainMenu(newGame));
+}
+
+mainMenu();
